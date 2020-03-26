9d616e1eca8978a6d51cd26cbe3fa4a8e10cc1bd
Josefin - elevdator <Josefin@DESKTOP-RRN9P3L>
2020-03-26 18:01:15 +0100

    funkar börjar byta ut repetetiv kod börjat med queue

---
+++ InputTest/ExamineInputTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkalProj_Datastrukturer_Minne;                    //referens fixad, using fixad, vad mer?
+
+namespace InputTest
+{
+    [TestClass]
+    public class ExamineInputTest
+    {
+        [TestMethod]
+        public void ExamineMethodInput_ReturnsNotNull()
+        {
+            //Arrange
+            //expected input = !null
+            const string expected = "hej";  //hur ska jag tänka? i vilket steg ska jag kolla?
+            //input = expected;
+
+            //Act
+            //put in input through code
+            //nav = input[0]
+
+            //Assert
+            //actual nav
+        }
+    }
+}
--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -57,50 +57,107 @@ namespace SkalProj_Datastrukturer_Minne
                 }
             }
         }
+        private static string GetInput(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            return input;
+        }
 
+        private static void ListStatus(List<string> collection, string value)
+        {
+            Console.Write($"Count: {collection.Count}\nValues:\n");
+            collection.ForEach(m => Console.WriteLine($"{m}")); //check list
+        }
+        private static bool CheckNull(string inputValue)
+        {
+            bool isNull=false;
+            if (string.IsNullOrEmpty(inputValue)) { 
+                Console.WriteLine("Please enter some valid input");
+                isNull = true;
+            } 
+
+            return isNull;
+        }
         /// <summary>
         /// Examines the datastructure List
         /// </summary>
         static void ExamineList()
         {
             /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch statement with cases '+' and '-'
-             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
-             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
-             * In both cases, look at the count and capacity of the list
-             * As a default case, tell them to use only + or -
-             * Below you can see some inspirational code to begin working.
+             * x Loop this method untill the user inputs something to exit to main menue.
+             * x Create a switch statement with cases '+' and '-'
+             * x '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * x '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * x ToDo: In both cases, look at the count and capacity of the list
+             * x As a default case, tell them to use only + or -
+             * x Below you can see some inspirational code to begin working.
             */
-            List<string> theList = new List<string>();
-            string input = Console.ReadLine();
-            //char nav = input[0];
-            char nav = ' ';
+
+            List<string> theList = new List<string>{ "one", "two", "three", "four" };
+
+            //ToDo: internal? npt when static?
+            char nav;
             string value;
             bool running = true;
 
             do
             {
-                nav = input[0];
-                value = input.Substring(1);
-                switch (nav)
-                {
-                    case '+': //add value to theList
-                        break;
-                    case '-': //remove value to theList
-                        break;
-                    case '0': running=false;//exit to main menu - bool=false
-                        break;
-                    default:
-                        Console.WriteLine("Please enter some valid input (+ or -)");
-                        break;
-                }
+                string input = GetInput("Add value with + or remove with - : ");
 
+                //nav = input[0];
+                //CheckString.NullCheck(input);
+                /*if (CheckNull(input))
+                {   //ToDo: make generic/method
+                    Console.WriteLine("Please enter some valid input (+ or -)");
+                }*/
+                if(!CheckNull(input))
+                {
+                    nav = input[0];
+                    value = input.Substring(1);
+                    switch (nav)
+                    {
+                        case '+':
+                            /*if (string.IsNullOrEmpty(value))
+                            {//ToDo: make generic/method
+                                Console.WriteLine("The input value is empty");
+                            }*/
+                            if (CheckNull(value))
+                            {                            //check value?
+                                theList.Add(value);         //add value to theList
+                                Console.WriteLine($"{value} added to the list\n");
+                            }
+                            ListStatus(theList, value);
+                            break;
+                        case '-':
+                            if (theList.Contains(value))    //if value exists in the list
+                            {
+                                theList.Remove(value);      //removes value (first of same values) to theList
+                                Console.WriteLine($"{value} removed from the list");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"'{value}' does not exist in the list!");
+                            }
+                            //variable = (condition) ? expressionTrue : expressionFalse;
+                            //string message = theList.Contains(value) ? $"{value} removed from the list" : $"'{value}' does not exist in the list!";
+                            //Console.WriteLine(message);
+                            ListStatus(theList, value);
+                            break;
+                        case '0':
+                            running = false;            //exit to main menu - bool=false
+                            break;
+                        default:
+                            Console.WriteLine("Please enter some valid input (+ or -)");
+                            break;
+                    }
+                }
+                //theList.ForEach(m => Console.WriteLine($"{m}, ")); //check list
             } while (running);
             
-
-            
         }
+
+        
 
         /// <summary>
         /// Examines the datastructure Queue
@@ -108,10 +165,56 @@ namespace SkalProj_Datastrukturer_Minne
         static void ExamineQueue()
         {
             /*
-             * Loop this method untill the user inputs something to exit to main menue.
+             * x Loop this method untill the user inputs something to exit to main menue.
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+            var theQueue = new Queue<string>();
+            theQueue.Enqueue("one");
+            theQueue.Enqueue("two");
+            theQueue.Enqueue("three");
+
+            //ToDo: make internal?
+            char nav;
+            string value;
+            bool running = true;
+            do
+            {
+                string input = GetInput("Enter + for enqueue and - for dequeue: ");
+                if (string.IsNullOrEmpty(input))
+                {   //ToDo: make generic
+                    Console.WriteLine("Please enter some valid input");
+                }
+                else
+                {
+                    nav = input[0];
+                    value = input.Substring(1);
+                    switch (nav)
+                    {
+                        case '+':
+                            theQueue.Enqueue(value);         
+                            Console.WriteLine($"{value} added to the queue\n");
+                            break;
+                        case '-': //ToDo: if queue empty
+                            string removedObj=theQueue.Dequeue();     //removes ""and returns"" obj at beginning
+                            Console.WriteLine($"{removedObj} removed from the queue\n");
+                            break;
+                        case '0':
+                            running = false;          
+                            break;
+                        default://ToDo: generic method
+                            Console.WriteLine("Please enter some valid input");
+                            break;
+                    }
+                }
+                var queueCopy = new string[theQueue.Count]; //Other way? only peek for first value
+                int n= 0;
+                theQueue.CopyTo(queueCopy,n);
+                foreach (var item in queueCopy)
+                {
+                    Console.WriteLine(item);
+                }
+            } while (running);
         }
 
         /// <summary>
@@ -124,6 +227,11 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+            bool running = true;
+            do
+            {
+                string input = GetInput("Add value with + or remove with - : ");
+            } while (running);
         }
 
         static void CheckParanthesis()
